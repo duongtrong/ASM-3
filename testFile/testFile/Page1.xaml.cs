27d744e49feb1eda95122e218bec4c8e42e4df77
duongtrong <trongdvd00543@fpt.edu.vn>
2018-10-25 20:00:57 +0700

    Thuc Hanh

---
+++ testFile/testFile/Page1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace testFile
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page1 : Page
+    {
+        public Page1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Save_File(object sender, RoutedEventArgs e)
+        {
+            save_file();
+        }
+
+        public async void save_file() {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile file = await folder.CreateFileAsync(this.fileName.Text + ".txt", CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(file, this.Content.Text);
+            Debug.WriteLine("done!");
+        }
+    }
+}
